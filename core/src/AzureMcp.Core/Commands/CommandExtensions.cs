b6780fdc536540274993a3f16865a07fde73a045
qianwens <qianwens@microsoft.com>
2025-08-05 14:47:13 +0800

    update plan command description

--- core/src/AzureMcp.Core/Commands/CommandExtensions.cs
+++ core/src/AzureMcp.Core/Commands/CommandExtensions.cs
@@ -95,7 +95,7 @@ public static class CommandExtensions
 
         return command.Parse(args.ToArray());
     }
-    
+
     /// <summary>
     /// Determines if an option expects a collection type that should be treated as an array
     /// </summary>
