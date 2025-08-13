44e40099fb84a205c4530e4f63712b872087549a
Jon Gallant <2163001+jongio@users.noreply.github.com>
2025-08-13 11:43:12 -0700

    Remove IRequireResourceGroup interface as it is no longer needed

--- core/src/AzureMcp.Core/Validation/IRequireResourceGroup.cs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-namespace AzureMcp.Core.Validation;
-
-// Marker interface indicating a command logically requires --resource-group
-public interface IRequireResourceGroup { }
